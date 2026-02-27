12ae53d857c37c4cf3d39cad1b08d30306dfe2e7
Jamie <2119834+jamieQ@users.noreply.github.com>
2026-02-27 15:51:46 -0600

    [feedback]: better regex & verbose options

--- utils/find-unused-diagnostics.sh
+++ utils/find-unused-diagnostics.sh
@@ -7,13 +7,13 @@ set -euo pipefail
 
 # Gather all diagnostic identifiers.
 ALL_DIAGS=$(
-    grep -Eho '(ERROR|WARNING|NOTE|REMARK)\([a-z_]+,' include/swift/AST/Diagnostics*.def \
-    | sed -E 's/(ERROR|WARNING|NOTE|REMARK)\(([a-z_]+),/\2/')
+    grep -E --only-matching --no-filename '(ERROR|WARNING|NOTE|REMARK)\([a-zA-Z0-9_]+,' include/swift/AST/Diagnostics*.def \
+    | sed -E 's/(ERROR|WARNING|NOTE|REMARK)\(([a-zA-Z0-9_]+),/\2/')
 
 # Now look for all potential identifiers in the (C++) source files.
 CXX_SOURCES=$(find lib include tools -name \*.cpp -or -name \*.h)
 DIAGS_IN_CXX_SOURCES=$(
-    grep -Ehoz 'diag::\n?\s*[a-z_]+' $CXX_SOURCES \
+    grep -E --only-matching --no-filename --null-data 'diag::\n?\s*[a-zA-Z0-9_]+' $CXX_SOURCES \
     | sed -e 's/diag:://' -e 's/[[:space:]]//g')
 
 # Get potentially unused diags from C++ sources.
@@ -22,7 +22,7 @@ POTENTIALLY_UNUSED=$(comm -23 <(sort -u <<< "$ALL_DIAGS") <(sort -u <<< "$DIAGS_
 # Finally, check if any of the possibly-unused diags appear in Swift sources, and exclude them.
 SWIFT_SOURCES=$(find SwiftCompilerSources -name \*.swift)
 if [ -n "$SWIFT_SOURCES" ] && [ -n "$POTENTIALLY_UNUSED" ]; then
-    DIAGS_IN_SWIFT=$(grep -Fho -f <(echo "$POTENTIALLY_UNUSED") $SWIFT_SOURCES)
+    DIAGS_IN_SWIFT=$(grep --fixed-strings --only-matching --no-filename --file <(echo "$POTENTIALLY_UNUSED") $SWIFT_SOURCES)
     comm -23 <(echo "$POTENTIALLY_UNUSED") <(sort -u <<< "$DIAGS_IN_SWIFT")
 else
     echo "$POTENTIALLY_UNUSED"
