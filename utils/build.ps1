0cef5bcd00c4f1d4864cb3df88d6da80159931d0
Evan Wilde <ewilde@apple.com>
2026-03-24 15:28:38 -0700

    Android: Update build.ps1 to use CMAKE_SYSROOT
    When cross-compiling, the toolchain file is supposed to set the
    `CMAKE_SYSROOT` variable, which
    In this case, there is no toolchain file to set it as build.ps1 is
    passing everything through explicitly.
    Setting it explicitly now for the C and C++ bits and for computing
    flags.
    
    CMake doesn't yet support passing `CMAKE_SYSROOT` to the Swift compiler
    via the `sysroot` flag though:
    https://gitlab.kitware.com/cmake/cmake/-/work_items/27717
    
    For now, we need to keep passing it as an explicit Swift flag.

--- utils/build.ps1
+++ utils/build.ps1
@@ -1747,6 +1747,7 @@ function Build-CMakeProject {
         Add-KeyValueIfNew $Defines CMAKE_ANDROID_API "$AndroidAPILevel"
         Add-KeyValueIfNew $Defines CMAKE_ANDROID_ARCH_ABI $Platform.Architecture.ABI
         Add-KeyValueIfNew $Defines CMAKE_ANDROID_NDK "$AndroidNDKPath"
+        Add-KeyValueIfNew $Defines CMAKE_SYSROOT "$AndroidSysroot"
 
         if ($UseASM) {
         }
@@ -1754,7 +1755,7 @@ function Build-CMakeProject {
         if ($UseC) {
           Add-KeyValueIfNew $Defines CMAKE_C_COMPILER_TARGET $Platform.Triple
 
-          $CFLAGS = @("--sysroot=${AndroidSysroot}", "-ffunction-sections", "-fdata-sections")
+          $CFLAGS = @("-ffunction-sections", "-fdata-sections")
           if ($DebugInfo) {
             $CFLAGS += @("-gdwarf")
           }
@@ -1764,7 +1765,7 @@ function Build-CMakeProject {
         if ($UseCXX) {
           Add-KeyValueIfNew $Defines CMAKE_CXX_COMPILER_TARGET $Platform.Triple
 
-          $CXXFLAGS = @("--sysroot=${AndroidSysroot}", "-ffunction-sections", "-fdata-sections")
+          $CXXFLAGS = @("-ffunction-sections", "-fdata-sections")
           if ($DebugInfo) {
             $CXXFLAGS += @("-gdwarf")
           }
@@ -1790,6 +1791,8 @@ function Build-CMakeProject {
           [string[]] $SwiftFlags = @()
 
           $SwiftFlags += if ($SwiftSDK) {
+            # TODO: CMake does not yet have support for passing `CMAKE_SYSROOT` to the Swift compiler yet.
+            #       Once we have that, we can drop `-sysroot $AndroidSysroot` here.
             @("-sdk", $SwiftSDK, "-sysroot", $AndroidSysroot)
           } else {
             @()
